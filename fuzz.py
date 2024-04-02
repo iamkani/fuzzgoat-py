77768513de09b41c401f7b211dd8990f6d15d0b7
iamkani <arunkaniyamattam@gmail.com>
2024-04-02 15:45:37 -0300

    Update fuzz.py

--- fuzz.py
+++ fuzz.py
@@ -14,7 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""This fuzzer is an example of looking for weird exceptions in ruamel.yaml.
+"""This fuzzer is an example of looking for weird exceptions in ruamel.yaml. Az
 
 While these do not necessarily indicate vulnerabilities, they often indicate
 other bugs in code.
